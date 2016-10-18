962dcbc0a510c83cdd346300772b6f6119942933
medevag <michaeledevag@gmail.com>
2016-10-18 17:08:16 +0200

    a

--- cd_changer_emulator.c
+++ cd_changer_emulator.c
@@ -199,7 +199,7 @@ void melbus_Init_CDCHRG() {
 	printf("Busy-wait");
 
 	// Wait until Busy-line goes high (not busy) before we pull BUSY low to request init
-	while(tst == 1){//digitalRead(MELBUS_BUSY)==LOW){
+	while(tst != 1){//digitalRead(MELBUS_BUSY)==LOW){
 		// Busy-wait
 		tst = digitalRead(MELBUS_BUSY);
 		//printf("%d\n", tst);//"WAITING\n");
